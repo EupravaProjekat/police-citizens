6d04cebaa342f27ca26609abcfc38c80192ad6e3
Sovlja <101639981+Sovlja@users.noreply.github.com>
2024-04-16 06:12:51 +0200

    Commit #1 - home-component design

---
+++ eUprava-front/src/app/home-guest/home-guest.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-guest',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-guest.component.html',
+  styleUrl: './home-guest.component.css'
+})
+export class HomeGuestComponent {
+
+}
